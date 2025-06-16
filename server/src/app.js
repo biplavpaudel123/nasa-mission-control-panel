bd86d0965f164c46c13c8aadd66eba3f30b7f116
Your Name <Biplav Paudel>
2025-06-17 00:11:20 +0545

    Added launches routes in backend.

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
+// const path = require('path');
 const morgan = require('morgan');
+
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
+
 const app= express();
 
 app.use(cors({
@@ -13,11 +16,8 @@ app.use(cors({
 app.use(morgan('combined'));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'..','public')))
+// app.use(express.static(path.join(__dirname,'..','public')))
 app.use(planetsRouter);
-
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','public','index.html'));
-})
+app.use(launchesRouter);
 
 module.exports=app;
---
+++ server/src/models/launches.model.js
@@ -0,0 +1,18 @@
+const launches = new Map();
+
+const launch={
+    flightnumber: 100,
+    launchDate: new Date('December 27,2030'),
+    mission: 'Kelper Exploration X',
+    rocket:'Explorer IS1',
+    target:'Kelper-442b',
+    customer: ['ZTM','NASA'],
+    upcoming: true,
+    success: true,
+};
+
+launches.set(launch.flightnumber,launch)
+
+module.exports={
+    launches,
+}
